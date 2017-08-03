16ec370289a1106c056668e8015f39ca1ab23ade
Eugene Lazutkin <elazutkin@researchnow.com>
2017-08-03 10:39:39 -0500

    Made new distro.

---
+++ dist/utils/getPath.js
@@ -0,0 +1,40 @@
+(function (global, factory) {
+	if (typeof define === "function" && define.amd) {
+		define(['exports'], factory);
+	} else if (typeof exports !== "undefined") {
+		factory(exports);
+	} else {
+		var mod = {
+			exports: {}
+		};
+		factory(mod.exports);
+		global.getPath = mod.exports;
+	}
+})(this, function (exports) {
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+		return typeof obj;
+	} : function (obj) {
+		return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+	};
+
+	var dereferable = { object: 1, function: 1 };
+
+	exports.default = function (obj, path) {
+		var delimiter = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '.';
+
+		if (typeof path == 'string') {
+			path = path ? path.split(delimiter) : [];
+		}
+		for (var i = 0; i < path.length; ++i) {
+			if (!obj || !dereferable[typeof obj === 'undefined' ? 'undefined' : _typeof(obj)]) return; // undefined
+			obj = obj[path[i]];
+		}
+		return obj;
+	};
+});
--- dist/utils/pumpEvent.js
+++ dist/utils/pumpEvent.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['exports', './debounce'], factory);
+    define(['exports', './debounce', './getPath'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('./debounce'));
+    factory(exports, require('./debounce'), require('./getPath'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.debounce);
-    global.pumpValue = mod.exports;
+    factory(mod.exports, global.debounce, global.getPath);
+    global.pumpEvent = mod.exports;
   }
-})(this, function (exports, _debounce) {
+})(this, function (exports, _debounce, _getPath) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
@@ -19,16 +19,19 @@
 
   var _debounce2 = _interopRequireDefault(_debounce);
 
+  var _getPath2 = _interopRequireDefault(_getPath);
+
   function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {
       default: obj
     };
   }
 
-  exports.default = function (source, target, attrName) {
+  exports.default = function (target, attrName) {
+    var path = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
     var ms = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 500;
     return (0, _debounce2.default)(function (e) {
-      target.setAttribute(attrName, (source || e.target).value);
+      target.setAttribute(attrName, (0, _getPath2.default)(e, path));
     }, ms);
   };
 });
