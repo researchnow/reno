e38d62c9bb98be5f31572f47dad2d13b90f71522
Eugene Lazutkin <elazutkin@researchnow.com>
2017-08-03 16:13:13 -0500

    Regenerated distro, updated dependencies, ready for release.

--- dist/utils/pumpEvent.js
+++ dist/utils/pumpEvent.js
@@ -1,23 +1,23 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['exports', './debounce', './getPath'], factory);
+    define(['exports', './delay', './getPath'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('./debounce'), require('./getPath'));
+    factory(exports, require('./delay'), require('./getPath'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.debounce, global.getPath);
+    factory(mod.exports, global.delay, global.getPath);
     global.pumpEvent = mod.exports;
   }
-})(this, function (exports, _debounce, _getPath) {
+})(this, function (exports, _delay, _getPath) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
 
-  var _debounce2 = _interopRequireDefault(_debounce);
+  var _delay2 = _interopRequireDefault(_delay);
 
   var _getPath2 = _interopRequireDefault(_getPath);
 
@@ -30,7 +30,8 @@
   exports.default = function (target, attrName) {
     var path = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
     var ms = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 500;
-    return (0, _debounce2.default)(function (e) {
+    var wrap = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : _delay2.default;
+    return wrap(function (e) {
       target.setAttribute(attrName, (0, _getPath2.default)(e, path));
     }, ms);
   };
--- dist/utils/pumpValue.js
+++ dist/utils/pumpValue.js
@@ -1,23 +1,23 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['exports', './debounce'], factory);
+    define(['exports', './delay'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('./debounce'));
+    factory(exports, require('./delay'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.debounce);
+    factory(mod.exports, global.delay);
     global.pumpValue = mod.exports;
   }
-})(this, function (exports, _debounce) {
+})(this, function (exports, _delay) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
 
-  var _debounce2 = _interopRequireDefault(_debounce);
+  var _delay2 = _interopRequireDefault(_delay);
 
   function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {
@@ -27,7 +27,8 @@
 
   exports.default = function (source, target, attrName) {
     var ms = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 500;
-    return (0, _debounce2.default)(function (e) {
+    var wrap = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : _delay2.default;
+    return wrap(function (e) {
       target.setAttribute(attrName, (source || e.target).value);
     }, ms);
   };
