7c4daf1583c3e0d360d54d7c4c831e2370dce256
Eugene Lazutkin <elazutkin@researchnow.com>
2017-12-05 15:05:23 -0600

    Fixed removing the popup on click.

--- src/components/reno-popup.js
+++ src/components/reno-popup.js
@@ -1,57 +1,62 @@
 import { openPopup, closePopup } from "./reno-popup-actions";
 
-(function() {
-	class RenoPopup extends HTMLElement {
-		connectedCallback () {
-			// listen to events on the entire component
-			switch(this.getAttribute('trigger')) {
-				case 'click':
-					this.handle = on(document, 'click', this);
-					break;
-				// case 'focus':
-				// 	this.handle = on(this, 'focus', this);
-				// 	break;
-				default:
-				// case 'mouseover':
-					this.handle = on.makeMultiHandle([
-						on(this, 'mouseover', this),
-						on(this, 'mouseout', this)
-					]);
-			}
-		}
-		disconnectedCallback () {
-			this.handle.remove();
-		}
-		handleEvent (e) {
-			const isOpen = document.querySelector('#reno-popup-container').classList.contains('open');
+let handleClick = null;
 
-			// handle events
-			const trigger = this.getAttribute('trigger') || 'mouseover';
-			if (trigger !== e.type && (trigger !== 'mouseover' || e.type !== 'mouseout')) return;
-
-			switch (e.type) {
-				case 'click':
-					// clicking anywhere except the popup container will close
-					isOpen && !on.closest(e.target, '#reno-popup-container') && closePopup();
-					// clicking only the popup component will open
-					!isOpen && this.contains(e.target) && openPopup(this);
-					break;
-				case 'focus':
-					// TODO
-					break;
-				case 'mouseover':
-					openPopup(this);
-					break;
-				case 'mouseout':
-					if (!e.relatedTarget || !on.closest(e.relatedTarget, '#reno-popup-container')) {
-						closePopup();
-					}
-					break;
-			}
-			e.stopPropagation();
-			e.stopImmediatePropagation();
-			// e.preventDefault();
+const hidePopup = e => {
+	const popup = document.getElementById('reno-popup-container');
+	if (popup) {
+		if (!popup.contains(e.target)) {
+			closePopup();
 		}
 	}
-	customElements.define('reno-popup', RenoPopup);
-})();
+};
+
+class RenoPopup extends HTMLElement {
+	connectedCallback () {
+		// listen to events on the entire component
+		switch(this.getAttribute('trigger')) {
+			case 'click':
+				this.handle = on(this, 'click', this);
+				if (!handleClick) {
+					handleClick = on(document, 'click', hidePopup);
+				}
+				break;
+			// case 'focus':
+			// 	this.handle = on(this, 'focus', this);
+			// 	break;
+			default:
+			// case 'mouseover':
+				this.handle = on(this, 'mouseover,mouseout', this);
+				break;
+		}
+	}
+	disconnectedCallback () {
+		this.handle.remove();
+	}
+	handleEvent (e) {
+		// handle events
+		const trigger = this.getAttribute('trigger') || 'mouseover';
+		if (trigger !== e.type && (trigger !== 'mouseover' || e.type !== 'mouseout')) return;
+
+		switch (e.type) {
+			case 'click':
+				openPopup(this);
+				break;
+			// case 'focus':
+			// 	// TODO
+			// 	break;
+			case 'mouseover':
+				openPopup(this);
+				break;
+			case 'mouseout':
+				const popup = document.getElementById('reno-popup-container');
+				if (!e.relatedTarget || popup && !popup.contains(e.relatedTarget)) {
+					closePopup();
+				}
+				break;
+		}
+		e.stopPropagation();
+	}
+}
+
+customElements.define('reno-popup', RenoPopup);
