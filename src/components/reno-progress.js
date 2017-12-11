379af16122724cde3e891ca350a16905e86c4b5c
tjiang <tianbi.jiang@gmail.com>
2017-12-11 14:36:52 -0600

    Progress bar done

--- src/components/reno-progress.js
+++ src/components/reno-progress.js
@@ -1,5 +1,6 @@
 class RenoProgress extends HTMLElement {
   connectedCallback() {
+    // create progress and bar elements
     this.progressElement = this.ownerDocument.createElement('div');
     this.progressElement.classList.add('reno-progress');
     this.barElement = this.ownerDocument.createElement('div');
@@ -10,26 +11,35 @@ class RenoProgress extends HTMLElement {
     this.updateProgress(this.getAttribute('progress'));
   }
 
-  static get observedAttributes() {return ['progress']; }
+  static get observedAttributes() { return ['progress']; }
 
   attributeChangedCallback(attrName, oldVal, newVal) {
     if (attrName === 'progress') this.updateProgress(newVal);
   }
 
   updateProgress(progress) {
+    if (!(this.barElement && this.progressElement)) return;
     switch(true) {
       case progress === 'pending':
       case progress === 'reverse-pending':
-        if (this.barElement) this.barElement.classList.add(progress);
+        this.barElement.classList.add(progress);
         break;
       case !isNaN(parseFloat(progress)):
-        if (this.barElement) this.barElement.style.width = parseFloat(progress) + 'px';
+        // clean up pending states
+        this.barElement.classList.remove('pending');
+        this.barElement.classList.remove('reverse-pending');
+        // set bar width
+        const progressWidth = parseFloat(getComputedStyle(this.progressElement).width);
+        const barWidth = progressWidth * parseFloat(progress) / 100;
+        this.barElement.style.width = barWidth + 'px';
+        // make the right end of the bar round
+        if (progressWidth - barWidth < 8) this.barElement.classList.add('round');
+        else this.barElement.classList.remove('round');
+        break;
+      default:
+        this.barElement.classList.add('pending');
         break;
     }
-  }
-
-  disconnectedCallback() {
-    // this.ownerDocument.removeChild(this.progressContainer)
   }
 }
 
