0bb54c13be8fc19d347126b8de3df2fd433b1cea
tjiang <tianbi.jiang@gmail.com>
2018-02-12 16:12:45 -0600

    Toggle updated - now can be collected as formData

--- src/components/reno-toggle.js
+++ src/components/reno-toggle.js
@@ -1,5 +1,11 @@
 class RenoToggle extends HTMLElement {
   connectedCallback() {
+    // create checkbox input
+    const input = this.ownerDocument.createElement('input');
+    input.type = 'checkbox';
+    input.name = this.getAttribute('name') || 'toggle';
+    input.checked = this.getAttribute('status') == 'on';
+    this.appendChild(input);
     this.handle = on(this, 'click', this);
   }
 
@@ -8,19 +14,22 @@ class RenoToggle extends HTMLElement {
   }
   
   handleEvent () {
+    const input = this.firstChild;
     switch (this.getAttribute('status')) {
       case 'on':
         this.setAttribute('status', 'off');
+        input.checked = false;
         break;
       case 'off':
       default:
         this.setAttribute('status', 'on');
+        input.checked = true;
         break;
     }
   }
 
-  // custom methods
-  get status ()     { return this.getAttribute('status') }
+  // // custom methods
+  // get status () { return this.getAttribute('status') }
 
 }
 
