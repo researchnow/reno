fa7137b30099af21174f4b5edcbc29e27e475a85
Eugene Lazutkin <elazutkin@researchnow.com>
2018-04-13 16:00:03 -0500

    Restructure toPrecision() + new version.

--- src/utils/toPrecision.js
+++ src/utils/toPrecision.js
@@ -2,15 +2,17 @@
 let eps = 1;
 for (let next = eps; next + 1 > 1; eps = next, next = 0.5 * next);
 
-export const epsilon = eps;
-
 /**
  * Quick helper function to convert a number to x number of decimal places
  * @param number
  * @param decimalPlaces
  */
-export const toPrecision = (number, decimalPlaces) => {
+const toPrecision = (number, decimalPlaces) => {
   number = number || 0;
   if (typeof number == "string") number = parseFloat(number);
-  return (number + number * epsilon).toFixed(decimalPlaces);
+  return parseFloat((number + number * toPrecision.epsilon).toFixed(decimalPlaces));
 }
+
+toPrecision.epsilon = eps;
+
+export default toPrecision;
