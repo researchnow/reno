012c9b02c4d1d55bf1666682d0836890b0160dce
Eugene Lazutkin <elazutkin@researchnow.com>
2018-05-25 10:04:24 -0500

    Removed hot loading. Added config files.

---
+++ scripts/build-font.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# THIS SCRIPT IS NON-WORKING AS OF YET
+
+# Install: pip install fonttools
+
+# pyftsubset ./Font\ Awesome/web-font-with-css/webfonts/fa-regular-400.ttf --unicodes=U+f0d9,U+ --output-file=./src/fonts/reno.ttf
+
+# solid arrows (caret): U+f0d7-f0da
+# solid arrows (angle): U+f104-f107
+# solid: U+f142
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,117 +4,107 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
-
 module.exports = {
-		devtool: 'cheap-module-source-map',
-    entry: [
-        'react-hot-loader/patch',
-        // activate HMR for React
+  devtool: 'cheap-module-source-map',
+  entry: [
+    // 'react-hot-loader/patch',
+    // activate HMR for React
 
-        'webpack-dev-server/client?http://localhost:8080',
-        // bundle the client for webpack-dev-server
-        // and connect to the provided endpoint
+    // 'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
 
-        'webpack/hot/only-dev-server',
-        // bundle the client for hot reloading
-        // only- means to only hot reload for successful updates
+    // 'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
 
-				resolve('src/scripts/main.js'),
-				resolve('src/scripts/app.js')
-        // the entry point of our app
-    ],
-    output: {
-        filename: 'main.js',
-        path: resolve('build'),
-				pathinfo: true,
-				chunkFilename: 'static/js/[name].chunk.js',
-        publicPath: '/',
-				devtoolModuleFilenameTemplate: info => resolve(info.absoluteResourcePath)
-    },
-		resolve: {
-			// This allows you to set a fallback for where Webpack should look for modules.
-			// We placed these paths second because we want `node_modules` to "win"
-			// if there are any conflicts. This matches Node resolution mechanism.
-			// https://github.com/facebookincubator/create-react-app/issues/253
-			modules: ['node_modules', resolve('node_modules')],
-			// These are the reasonable defaults supported by the Node ecosystem.
-			// We also include JSX as a common component filename extension to support
-			// some tools, although we do not recommend using it, see:
-			// https://github.com/facebookincubator/create-react-app/issues/290
-			extensions: ['.js', '.json', '.jsx'],
-			alias: {
+    resolve('src/scripts/main.js'),
+    resolve('src/scripts/app.js')
+    // the entry point of our app
+  ],
+  output: {
+    filename: 'main.js',
+    path: resolve('build'),
+    pathinfo: true,
+    chunkFilename: 'static/js/[name].chunk.js',
+    publicPath: '/',
+    devtoolModuleFilenameTemplate: info => resolve(info.absoluteResourcePath)
+  },
+  resolve: {
+    // This allows you to set a fallback for where Webpack should look for modules.
+    // We placed these paths second because we want `node_modules` to "win"
+    // if there are any conflicts. This matches Node resolution mechanism.
+    // https://github.com/facebookincubator/create-react-app/issues/253
+    modules: ['node_modules', resolve('node_modules')],
+    // These are the reasonable defaults supported by the Node ecosystem.
+    // We also include JSX as a common component filename extension to support
+    // some tools, although we do not recommend using it, see:
+    // https://github.com/facebookincubator/create-react-app/issues/290
+    extensions: ['.js', '.json', '.jsx'],
+    alias: {
+      // Support React Native Web
+      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      'react-native': 'react-native-web'
+    }
+  },
+  devServer: {
+    // hot: true,
+    // enable HMR on the server
 
-				// Support React Native Web
-				// https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-				'react-native': 'react-native-web'
-			}
-		},
-    devServer: {
-        hot: true,
-        // enable HMR on the server
+    contentBase: resolve('.'),
+    // match the output path
 
-        contentBase: resolve('.'),
-        // match the output path
-
-        publicPath: '/'
-        // match the output `publicPath`
-    },
-    module: {
-				strictExportPresence: true,
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: ['babel-loader'],
-                exclude: /node_modules/
-            },
-						{
-							test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-							loader: require.resolve('url-loader'),
-							options: {
-								limit: 10000,
-								name: 'media/[name].[hash:8].[ext]',
-							}
-						},
-            {
-                test: /\.scss$/,
-                use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-                        use: [
-                            {
-                                loader: "css-loader" // translates CSS into CommonJS
-                            },
-                            {
-                                loader: "sass-loader" // compiles Sass to CSS
-                            }
-                        ],
-                        fallback: "style-loader" // used when css not extracted
-                    }
-                ))
-            },
-            {
-                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-                use: 'url-loader'
-						}
-        ]
-    },
-    plugins: [
-				new HtmlWebpackPlugin({
-						inject: true,
-						template: resolve('src/index.html'),
-				}),
-
-        new webpack.HotModuleReplacementPlugin(),
-        // enable HMR globally
-
-				// Watcher doesn't work well if you mistype casing in a path so we use
-		    // a plugin that prints an error when you attempt to do this.
-		    // See https://github.com/facebookincubator/create-react-app/issues/240
-		    new CaseSensitivePathsPlugin(),
-
-        new webpack.NamedModulesPlugin(),
-        // prints more readable module names in the browser console on HMR updates
-
-        new ExtractTextPlugin({filename: 'styles.css', allChunks: true}),
-
-				new webpack.IgnorePlugin(/caniuse-lite\/data\/regions/)
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'media/[name].[hash:8].[ext]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader' // used when css not extracted
+        })
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: 'url-loader'
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: resolve('src/index.html')
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    // Watcher doesn't work well if you mistype casing in a path so we use
+    // a plugin that prints an error when you attempt to do this.
+    // See https://github.com/facebookincubator/create-react-app/issues/240
+    new CaseSensitivePathsPlugin(),
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new ExtractTextPlugin({filename: 'styles.css', allChunks: true}),
+
+    new webpack.IgnorePlugin(/caniuse-lite\/data\/regions/)
+  ]
 };
