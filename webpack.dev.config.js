2c06408e81058cf97bfb27c044ad64e5231ef4fb
Eugene Lazutkin <elazutkin@researchnow.com>
2018-06-08 15:14:28 -0500

    Updated dependencies.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,110 +1,109 @@
 const {resolve} = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
-  entry: [
-    // 'react-hot-loader/patch',
-    // activate HMR for React
+	devtool: 'cheap-module-source-map',
+	entry: [
+		// 'react-hot-loader/patch',
+		// activate HMR for React
 
-    // 'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
+		// 'webpack-dev-server/client?http://localhost:8080',
+		// bundle the client for webpack-dev-server
+		// and connect to the provided endpoint
 
-    // 'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
+		// 'webpack/hot/only-dev-server',
+		// bundle the client for hot reloading
+		// only- means to only hot reload for successful updates
 
-    resolve('src/scripts/main.js'),
-    resolve('src/scripts/app.js')
-    // the entry point of our app
-  ],
-  output: {
-    filename: 'main.js',
-    path: resolve('build'),
-    pathinfo: true,
-    chunkFilename: 'static/js/[name].chunk.js',
-    publicPath: '/',
-    devtoolModuleFilenameTemplate: info => resolve(info.absoluteResourcePath)
-  },
-  resolve: {
-    // This allows you to set a fallback for where Webpack should look for modules.
-    // We placed these paths second because we want `node_modules` to "win"
-    // if there are any conflicts. This matches Node resolution mechanism.
-    // https://github.com/facebookincubator/create-react-app/issues/253
-    modules: ['node_modules', resolve('node_modules')],
-    // These are the reasonable defaults supported by the Node ecosystem.
-    // We also include JSX as a common component filename extension to support
-    // some tools, although we do not recommend using it, see:
-    // https://github.com/facebookincubator/create-react-app/issues/290
-    extensions: ['.js', '.json', '.jsx'],
-    alias: {
-      // Support React Native Web
-      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web'
-    }
-  },
-  devServer: {
-    // hot: true,
-    // enable HMR on the server
+		resolve('src/scripts/main.js'),
+		resolve('src/scripts/app.js')
+		// the entry point of our app
+	],
+	output: {
+		filename: 'main.js',
+		path: resolve('build'),
+		pathinfo: true,
+		chunkFilename: 'static/js/[name].chunk.js',
+		publicPath: '/',
+		devtoolModuleFilenameTemplate: info => resolve(info.absoluteResourcePath)
+	},
+	resolve: {
+		// This allows you to set a fallback for where Webpack should look for modules.
+		// We placed these paths second because we want `node_modules` to "win"
+		// if there are any conflicts. This matches Node resolution mechanism.
+		// https://github.com/facebookincubator/create-react-app/issues/253
+		modules: ['node_modules', resolve('node_modules')],
+		// These are the reasonable defaults supported by the Node ecosystem.
+		// We also include JSX as a common component filename extension to support
+		// some tools, although we do not recommend using it, see:
+		// https://github.com/facebookincubator/create-react-app/issues/290
+		extensions: ['.js', '.json', '.jsx'],
+		alias: {
+			// Support React Native Web
+			// https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+			'react-native': 'react-native-web'
+		}
+	},
+	devServer: {
+		// hot: true,
+		// enable HMR on the server
 
-    contentBase: resolve('.'),
-    // match the output path
+		contentBase: resolve('.'),
+		// match the output path
 
-    publicPath: '/'
-    // match the output `publicPath`
-  },
-  module: {
-    strictExportPresence: true,
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          name: 'media/[name].[hash:8].[ext]'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'sass-loader'],
-          fallback: 'style-loader' // used when css not extracted
-        })
-      },
-      {
-        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-        use: 'url-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: resolve('src/index.html')
-    }),
+		publicPath: '/'
+		// match the output `publicPath`
+	},
+	module: {
+		strictExportPresence: true,
+		rules: [
+			{
+				test: /\.jsx?$/,
+				use: ['babel-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+				loader: require.resolve('url-loader'),
+				options: {
+					limit: 10000,
+					name: 'media/[name].[hash:8].[ext]'
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+				use: 'url-loader'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: resolve('src/index.html')
+		}),
 
-    new webpack.HotModuleReplacementPlugin(),
-    // enable HMR globally
+		new webpack.HotModuleReplacementPlugin(),
+		// enable HMR globally
 
-    // Watcher doesn't work well if you mistype casing in a path so we use
-    // a plugin that prints an error when you attempt to do this.
-    // See https://github.com/facebookincubator/create-react-app/issues/240
-    new CaseSensitivePathsPlugin(),
+		// Watcher doesn't work well if you mistype casing in a path so we use
+		// a plugin that prints an error when you attempt to do this.
+		// See https://github.com/facebookincubator/create-react-app/issues/240
+		new CaseSensitivePathsPlugin(),
 
-    new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
+		new webpack.NamedModulesPlugin(),
+		// prints more readable module names in the browser console on HMR updates
 
-    new ExtractTextPlugin({filename: 'styles.css', allChunks: true}),
+		new MiniCssExtractPlugin({
+			filename: 'styles.css'
+		}),
 
-    new webpack.IgnorePlugin(/caniuse-lite\/data\/regions/)
-  ]
+		new webpack.IgnorePlugin(/caniuse-lite\/data\/regions/)
+	]
 };
