6646448bfbe2049e27e3f109e4c8489079d74ed4
Eugene Lazutkin <elazutkin@researchnow.com>
2018-10-03 10:42:56 -0500

    Regenerated dependencies. New version: 1.9.0.

--- dist/components/reno-form.js
+++ dist/components/reno-form.js
@@ -11,204 +11,268 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 (function () {
-	'use strict';
+  'use strict';
 
-	var supportedEvents = { click: 'onClick', input: 'onInput', submit: 'onSubmit' };
+  var supportedEvents = { click: 'onClick', input: 'onInput', submit: 'onSubmit' };
 
-	var validityFlags = ['customError', 'patternMismatch', 'rangeOverflow', 'rangeUnderFlow', 'stepMismatch', 'tooLong', 'typeMismatch', 'valid', 'valueMissing'];
+  var validityFlags = ['customError', 'patternMismatch', 'rangeOverflow', 'rangeUnderFlow', 'stepMismatch', 'tooLong', 'typeMismatch', 'valid', 'valueMissing'];
 
-	var RenoForm = function (_HTMLElement) {
-		_inherits(RenoForm, _HTMLElement);
+  var RenoForm = function (_HTMLElement) {
+    _inherits(RenoForm, _HTMLElement);
 
-		function RenoForm() {
-			_classCallCheck(this, RenoForm);
+    function RenoForm() {
+      _classCallCheck(this, RenoForm);
 
-			return _possibleConstructorReturn(this, (RenoForm.__proto__ || Object.getPrototypeOf(RenoForm)).apply(this, arguments));
-		}
+      return _possibleConstructorReturn(this, (RenoForm.__proto__ || Object.getPrototypeOf(RenoForm)).apply(this, arguments));
+    }
 
-		_createClass(RenoForm, [{
-			key: 'connectedCallback',
+    _createClass(RenoForm, [{
+      key: 'connectedCallback',
 
-			// life-cycle callbacks
-			value: function connectedCallback() {
-				var _this2 = this;
+      // life-cycle callbacks
+      value: function connectedCallback() {
+        var _this2 = this;
 
-				this.handle = on.makeMultiHandle(Object.keys(supportedEvents).map(function (eventName) {
-					_newArrowCheck(this, _this2);
+        this.handle = on.makeMultiHandle(Object.keys(supportedEvents).map(function (eventName) {
+          _newArrowCheck(this, _this2);
 
-					return on(this, eventName, this);
-				}.bind(this)));
-				if (this.getAttribute('showmessages') !== null) {
-					this.deferredFunction = this.showMessages;
-					window.requestAnimationFrame(function () {
-						_newArrowCheck(this, _this2);
+          return on(this, eventName, this);
+        }.bind(this)));
+        if (this.getAttribute('showmessages') !== null) {
+          this.deferredFunction = this.showMessages;
+          window.requestAnimationFrame(function () {
+            _newArrowCheck(this, _this2);
 
-						this.deferredFunction && this.deferredFunction.call(this);
-						this.deferredFunction = null;
-					}.bind(this));
-				}
-			}
-		}, {
-			key: 'disconnectedCallback',
-			value: function disconnectedCallback() {
-				this.handle.remove();
-				this.deferredFunction = null;
-			}
-		}, {
-			key: 'attributeChangedCallback',
-			value: function attributeChangedCallback(attrName, oldVal, newVal) {
-				if (attrName === 'disabled') {
-					var fieldSets = this.querySelectorAll('fieldset'),
-					    disabled = newVal !== null;
-					for (var i = 0; i < fieldSets.length; ++i) {
-						fieldSets[i].disabled = disabled;
-					}
-				}
-			}
-			// custom methods
+            this.deferredFunction && this.deferredFunction.call(this);
+            this.deferredFunction = null;
+          }.bind(this));
+        }
+      }
+    }, {
+      key: 'disconnectedCallback',
+      value: function disconnectedCallback() {
+        this.handle.remove();
+        this.deferredFunction = null;
+      }
+    }, {
+      key: 'attributeChangedCallback',
+      value: function attributeChangedCallback(attrName, oldVal, newVal) {
+        if (attrName === 'disabled') {
+          var fieldSets = this.querySelectorAll('fieldset'),
+              disabled = newVal !== null;
+          for (var i = 0; i < fieldSets.length; ++i) {
+            fieldSets[i].disabled = disabled;
+          }
+        }
+      }
+      // custom methods
 
-		}, {
-			key: 'getElements',
-			value: function getElements(clean) {
-				var elements = [],
-				    forms = this.querySelectorAll('form');
-				for (var i = 0; i < forms.length; ++i) {
-					var form = forms[i];
-					for (var j = 0; j < form.length; ++j) {
-						var element = form.elements[j];
-						if (!clean || element.name && (element.tagName.toLowerCase() === 'input' && (element.type === 'checkbox' || element.type === 'radio') ? element.checked : true)) {
-							elements.push(element);
-						}
-					}
-				}
-				return elements;
-			}
-		}, {
-			key: 'getElementsByName',
-			value: function getElementsByName(clean) {
-				var _this3 = this;
+    }, {
+      key: 'getElements',
+      value: function getElements(clean, skipButtons) {
+        var elements = [],
+            forms = this.querySelectorAll('form');
+        for (var i = 0; i < forms.length; ++i) {
+          var form = forms[i];
+          for (var j = 0; j < form.length; ++j) {
+            var element = form.elements[j];
+            if (!element.name) continue;
+            var tag = element.tagName.toLowerCase(),
+                type = element.type;
+            if (skipButtons && (tag === 'button' || type === 'submit' || type === 'reset')) continue;
+            if (clean && tag === 'input' && (type === 'radio' || type === 'checkbox') ? !element.checked : false) continue;
+            elements.push(element);
+          }
+        }
+        return elements;
+      }
+    }, {
+      key: 'getElementsByName',
+      value: function getElementsByName(clean, skipButtons) {
+        var _this3 = this;
 
-				var dict = {};
-				this.getElements(clean).forEach(function (element) {
-					_newArrowCheck(this, _this3);
+        var dict = {};
+        this.getElements(clean, skipButtons).forEach(function (element) {
+          _newArrowCheck(this, _this3);
 
-					return dict[element.name] = element;
-				}.bind(this));
-				return dict;
-			}
-		}, {
-			key: 'getFormData',
-			value: function getFormData() {
-				var form = this.querySelector('form');
-				return form && new FormData(form);
-			}
-		}, {
-			key: 'checkValidity',
-			value: function checkValidity() {
-				var forms = this.querySelectorAll('form');
-				var allValid = true;
-				for (var i = 0; i < forms.length; ++i) {
-					var valid = forms[i].checkValidity();
-					allValid = allValid && valid;
-				}
-				return allValid;
-			}
-		}, {
-			key: 'reportValidity',
-			value: function reportValidity() {
-				var forms = this.querySelectorAll('form');
-				var allValid = true;
-				for (var i = 0; i < forms.length; ++i) {
-					var valid = forms[i].reportValidity();
-					allValid = allValid && valid;
-				}
-				return allValid;
-			}
-		}, {
-			key: 'showElementMessages',
-			value: function showElementMessages(node, rootSelector, errorSelector) {
-				var _this4 = this;
+          return dict[element.name] = element;
+        }.bind(this));
+        return dict;
+      }
+    }, {
+      key: 'getValues',
+      value: function getValues(skipButtons) {
+        var _this4 = this;
 
-				var element = node;
-				if (rootSelector) {
-					while (element && element[on.matches] && !element[on.matches](rootSelector)) {
-						element = element.parentNode;
-					}element = element && element[on.matches] ? element : node;
-				}
-				validityFlags.forEach(function (flag) {
-					_newArrowCheck(this, _this4);
+        var form = {};
+        this.getElements(true, skipButtons).forEach(function (element) {
+          _newArrowCheck(this, _this4);
 
-					element.classList[node.validity[flag] ? 'add' : 'remove']('validity-' + flag);
-				}.bind(this));
-				if (rootSelector && errorSelector) {
-					var errorNode = element.querySelector(errorSelector);
-					if (errorNode) {
-						while (errorNode.lastChild) {
-							errorNode.removeChild(errorNode.lastChild);
-						}errorNode.appendChild(node.ownerDocument.createTextNode(node.validationMessage));
-					}
-				}
-				return this;
-			}
-		}, {
-			key: 'showFormMessages',
-			value: function showFormMessages(form, rootSelector, errorSelector) {
-				var elements = form.elements,
-				    length = form.length;
-				for (var j = 0; j < length; ++j) {
-					this.showElementMessages(elements[j], rootSelector, errorSelector);
-				}
-				return this;
-			}
-		}, {
-			key: 'showMessages',
-			value: function showMessages() {
-				var forms = this.querySelectorAll('form'),
-				    rootSelector = this.getAttribute('rootselector'),
-				    errorSelector = this.getAttribute('errorselector');
-				for (var i = 0; i < forms.length; ++i) {
-					this.showFormMessages(forms[i], rootSelector, errorSelector);
-				}
-				return this;
-			}
-			// event handlers
+          if (!element.name) return; // skip technical nameless form elements, like fieldsets
+          switch (element.type && element.type.toLowerCase()) {
+            case 'radio':
+              if (element.checked) {
+                form[element.name] = element.value;
+              }
+              break;
+            case 'checkbox':
+              if (element.checked) {
+                if (!(form[element.name] instanceof Array)) {
+                  form[element.name] = [];
+                }
+                form[element.name].push(element.value);
+              }
+              break;
+            default:
+              form[element.name] = element.value;
+              break;
+          }
+        }.bind(this));
+        return form;
+      }
+    }, {
+      key: 'setValues',
+      value: function setValues(form) {
+        var _this5 = this;
 
-		}, {
-			key: 'handleEvent',
-			value: function handleEvent(e) {
-				this[supportedEvents[e.type]](e);
-			}
-		}, {
-			key: 'onClick',
-			value: function onClick(e) {
-				if (e.target[on.matches]('input[type="checkbox"], input[type="radio"]')) {
-					this.dispatchEvent(new CustomEvent('reno-form-click', { bubbles: true, detail: { changed: e } }));
-				}
-			}
-		}, {
-			key: 'onInput',
-			value: function onInput(e) {
-				this.showElementMessages(e.target, this.getAttribute('rootselector'), this.getAttribute('errorselector'));
-				this.dispatchEvent(new CustomEvent('reno-form-input', { bubbles: true, detail: { changed: e } }));
-			}
-		}, {
-			key: 'onSubmit',
-			value: function onSubmit(e) {
-				if (e.target[on.matches]('form')) {
-					e.preventDefault();
-					this.showFormMessages(e.target, this.getAttribute('rootselector'), this.getAttribute('errorselector'));
-					this.dispatchEvent(new CustomEvent('reno-form-submit', { bubbles: true, detail: { form: e, validity: e.target.reportValidity() } }));
-				}
-			}
-		}], [{
-			key: 'observedAttributes',
-			get: function get() {
-				return ['disabled'];
-			}
-		}]);
+        this.getElements().forEach(function (element) {
+          _newArrowCheck(this, _this5);
 
-		return RenoForm;
-	}(HTMLElement);
+          if (form.hasOwnProperty(element.name)) {
+            switch (element.type && element.type.toLowerCase()) {
+              case 'radio':
+                element.checked = element.value === form[element.name];
+                break;
+              case 'checkbox':
+                var values = form[element.name];
+                if (values) {
+                  element.checked = values.some(function (value) {
+                    _newArrowCheck(this, _this5);
 
-	customElements.define('reno-form', RenoForm);
+                    return value === element.value;
+                  }.bind(this));
+                }
+                break;
+              default:
+                element.value = form[element.name];
+                break;
+            }
+          }
+        }.bind(this));
+      }
+    }, {
+      key: 'getFormData',
+      value: function getFormData() {
+        var form = this.querySelector('form');
+        return form && new FormData(form);
+      }
+    }, {
+      key: 'checkValidity',
+      value: function checkValidity() {
+        var forms = this.querySelectorAll('form');
+        var allValid = true;
+        for (var i = 0; i < forms.length; ++i) {
+          var valid = forms[i].checkValidity();
+          allValid = allValid && valid;
+        }
+        return allValid;
+      }
+    }, {
+      key: 'reportValidity',
+      value: function reportValidity() {
+        var forms = this.querySelectorAll('form');
+        var allValid = true;
+        for (var i = 0; i < forms.length; ++i) {
+          var valid = forms[i].reportValidity();
+          allValid = allValid && valid;
+        }
+        return allValid;
+      }
+    }, {
+      key: 'showElementMessages',
+      value: function showElementMessages(node, rootSelector, errorSelector) {
+        var _this6 = this;
+
+        var element = node;
+        if (rootSelector) {
+          while (element && element[on.matches] && !element[on.matches](rootSelector)) {
+            element = element.parentNode;
+          }element = element && element[on.matches] ? element : node;
+        }
+        validityFlags.forEach(function (flag) {
+          _newArrowCheck(this, _this6);
+
+          element.classList[node.validity[flag] ? 'add' : 'remove']('validity-' + flag);
+        }.bind(this));
+        if (rootSelector && errorSelector) {
+          var errorNode = element.querySelector(errorSelector);
+          if (errorNode) {
+            while (errorNode.lastChild) {
+              errorNode.removeChild(errorNode.lastChild);
+            }errorNode.appendChild(node.ownerDocument.createTextNode(node.validationMessage));
+          }
+        }
+        return this;
+      }
+    }, {
+      key: 'showFormMessages',
+      value: function showFormMessages(form, rootSelector, errorSelector) {
+        var elements = form.elements,
+            length = form.length;
+        for (var j = 0; j < length; ++j) {
+          this.showElementMessages(elements[j], rootSelector, errorSelector);
+        }
+        return this;
+      }
+    }, {
+      key: 'showMessages',
+      value: function showMessages() {
+        var forms = this.querySelectorAll('form'),
+            rootSelector = this.getAttribute('rootselector'),
+            errorSelector = this.getAttribute('errorselector');
+        for (var i = 0; i < forms.length; ++i) {
+          this.showFormMessages(forms[i], rootSelector, errorSelector);
+        }
+        return this;
+      }
+      // event handlers
+
+    }, {
+      key: 'handleEvent',
+      value: function handleEvent(e) {
+        this[supportedEvents[e.type]](e);
+      }
+    }, {
+      key: 'onClick',
+      value: function onClick(e) {
+        if (e.target[on.matches]('input[type="checkbox"], input[type="radio"]')) {
+          this.dispatchEvent(new CustomEvent('reno-form-click', { bubbles: true, detail: { changed: e } }));
+        }
+      }
+    }, {
+      key: 'onInput',
+      value: function onInput(e) {
+        this.showElementMessages(e.target, this.getAttribute('rootselector'), this.getAttribute('errorselector'));
+        this.dispatchEvent(new CustomEvent('reno-form-input', { bubbles: true, detail: { changed: e } }));
+      }
+    }, {
+      key: 'onSubmit',
+      value: function onSubmit(e) {
+        if (e.target[on.matches]('form')) {
+          e.preventDefault();
+          this.showFormMessages(e.target, this.getAttribute('rootselector'), this.getAttribute('errorselector'));
+          this.dispatchEvent(new CustomEvent('reno-form-submit', { bubbles: true, detail: { form: e, validity: e.target.reportValidity() } }));
+        }
+      }
+    }], [{
+      key: 'observedAttributes',
+      get: function get() {
+        return ['disabled'];
+      }
+    }]);
+
+    return RenoForm;
+  }(HTMLElement);
+
+  customElements.define('reno-form', RenoForm);
 })();
