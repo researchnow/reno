dacd7a7dde2035486c7d8c8e6e04e9d702089241
Eugene Lazutkin <elazutkin@researchnow.com>
2018-10-15 16:10:01 -0500

    Restructured dependencies, regenerated the demo, new version: 1.10.2.

--- src/scripts/deps.js
+++ src/scripts/deps.js
@@ -13,21 +13,11 @@ try {
 require('@clubajax/custom-elements-polyfill');
 
 import hyperHTML from 'hyperhtml/esm';
-
 if (!window.hyperHTML) {
   window.hyperHTML = hyperHTML;
 }
-// const hyperHTML = require('hyperhtml/cjs');
-// if (!window.hyperHTML) {
-//   window.hyperHTML = hyperHTML;
-// }
 
-window.customLoader = f => {
-  window.on = f();
-  window.customLoader = null;
-};
-require('@clubajax/on');
-// const on = require('@clubajax/on');
-// if (!window.on) {
-//   window.on = on;
-// }
+const on = require('@clubajax/on');
+if (!window.on) {
+  window.on = on;
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,2 @@
 import './deps';
 import './reno';
-import './components';
